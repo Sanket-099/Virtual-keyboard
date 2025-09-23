162712fb3571976fb5e9b7c4c71450d04cba6f92
Sanket-099 <sanketgaikwad1462005@gmail.com>
2025-09-23 23:13:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+const body = document.body;
+const text = document.getElementById("text");
+
+text.innerText = "";
+
+function getKeyElement(event) {
+  let keyIdentifier;
+  switch (event.key) {
+    case ' ':
+      keyIdentifier = 'Space';
+      break;
+    case '\\':
+      keyIdentifier = 'Backslash';
+      break;
+    default:
+      keyIdentifier = event.key;
+      break;
+  }
+  return document.getElementById(keyIdentifier);
+}
+
+body.addEventListener("keydown", function(event) {
+  const keyElement = getKeyElement(event);
+
+  if (keyElement) {
+    keyElement.classList.add("active");
+  }
+
+  switch (event.key) {
+    case "Backspace":
+      text.innerText = text.innerText.slice(0, -1);
+      break;
+    case "Enter":
+      text.innerText += '\n';
+      break;
+    case "Tab":
+      text.innerText += '\t';
+      break;
+    case "CapsLock":
+    case "Shift":
+    case "Control":
+    case "Alt":
+    case "Meta": 
+    case "Escape":
+    case "ArrowUp":
+    case "ArrowDown":
+    case "ArrowLeft":
+    case "ArrowRight":
+      break;
+    default:
+      text.innerText += event.key;
+      break;
+  }
+});
+
+body.addEventListener("keyup", function(event) {
+  const keyElement = getKeyElement(event);
+
+  if (keyElement) {
+    keyElement.classList.remove("active");
+  }
+});
+
+
+body.addEventListener("keydown", function (event) {
+  console.log(event.key);
+  const key = document.getElementById((event.key));
+  if (key) {
+    key.style.backgroundColor = "black";
+    key.style.color = "white";
+  }
+});
+
+body.addEventListener("keyup", function (event) {
+  const key = document.getElementById(event.key);
+  if (key) {
+    key.style.backgroundColor = "white";
+    key.style.color = "black";
+  }
+});
